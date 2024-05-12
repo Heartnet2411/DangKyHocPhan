a6570e9b986f230d018be78fc241061fb52aba35
vinhle009 <vinhthele1508@gmail.com>
2024-05-13 03:23:52 +0700

    done login

---
+++ frontend-dkhp/src/navigation/navigation.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Login } from '../srceens/Login/Login'
+import Home from '../srceens/Home/Home'
+const Navigation=()=>{
+    return(
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/home" element={<Home/>}/>
+            </Routes>
+        </Router>
+    )
+}
+export default Navigation
---
+++ frontend-dkhp/src/srceens/Home/Home.jsx
@@ -0,0 +1,36 @@
+// Main.js
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <header>
+                <div>
+                    <a href="/">
+                        <img src="https://media.iuh.edu.vn/Media/2_sviuh/Images/logo-svd516f114-e-e.png" />
+                    </a>
+                </div>
+                <div>
+                    <form>
+                        <input type="text" placeholder="Tìm kiếm..." required />
+                        <button type="submit">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="#">Xem thông tin </a>
+                        </li>
+                        <li>
+                            <a href="#">Đăng ký học phần </a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Home
